b1db961581ce4777b2dc5dd5ffc1375fab461142
julianthant <julianthant@gmail.com>
2024-01-20 03:54:50 -0800

    Update astro.config.mjs with new site and base URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
-import netlify from '@astrojs/netlify/functions';
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  output: 'server',
-  adapter: netlify(),
-  vite: {
-    ssr: {
-      external: ['image-size', 'tiny-glob'],
-    },
-  },
+  site: 'https://astronaut.github.io',
+  base: '/my-repo',
 });
